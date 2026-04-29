863c15923540197f6be9ee4693d8f834f1ec52e2
BYTEGUARDIAN14 <jmmunnerahmed@gmail.com>
2026-04-29 11:56:53 +0530

    Add ALB module main.tf

---
+++ modules/alb/main.tf
@@ -0,0 +1,40 @@
+resource "aws_lb" "this" {
+  name               = var.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.security_groups
+  subnets            = var.subnets
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = var.alb_name
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Not Found"
+      status_code  = "404"
+    }
+  }
+}
+
+output "alb_arn" {
+  value = aws_lb.this.arn
+}
+
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "listener_arn" {
+  value = aws_lb_listener.http.arn
+}
