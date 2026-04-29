291b89ea8aef45449d14c4179dc6ab5c6c10395e
BYTEGUARDIAN14 <jmmunnerahmed@gmail.com>
2026-04-29 11:56:56 +0530

    Add VPC module variables

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
